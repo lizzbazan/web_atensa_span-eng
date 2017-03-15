6663e97399a6ebec21df9e98a9e60b8614a1c484
Lizzeth Bazan <lizzbaz_1194@hotmail.com>
2017-03-15 17:26:42 -0600

    php
    elementos fijos

---
+++ fijos.php
@@ -0,0 +1,164 @@
+<?php
+function menures(){
+	echo '
+
+                  
+                  
+
+	';
+}
+
+
+function menu(){
+	echo '
+<section class="contenido-centrado">
+    <div id="logo">
+        <img src="img/log2.png"  alt=""/>
+    </div>
+    
+	<div id="cssmenu">
+	
+	          <ul >
+                <li><a href="index.php">Incio</a></li>
+				<li><a href="quienes.php">Nosotros</a></li>
+                <li class="active has-sub"><a href="#" >Productos</a>
+                <ul>
+                <li><a href="productos-prov1.php" >Retex</a></li>
+                <li><a href="productos-prov4.php" >Lana mineral</a></li>
+                <li><a href="productos-prov2.php" >fibra de vidrio </a></li>
+				
+                <li><a href="productos-prov3.php" >Fibra cerámica</a></li>
+                <li><a href="productos-prov5.php" >Vidrio celular</a></li>
+                
+                </ul>
+                </li>
+				 <li><a href="proyectos.php" >Proyectos</a>
+                
+                <li><a href="contacto.php" >Contacto</a></li>
+                <li><a href="faqs-es.php"> FAQs </a></li> 
+                <li><a href="index.php"><img src="img/bmexico.png" width="30px" height="30px" ></a></li>
+                 <li><a href="index-eng.php"><img src="img/busa.png" width="30px" height="30px" ></a></li>
+
+                
+                
+                </ul>
+	
+
+</div>
+	
+
+    </section>
+	';
+}
+
+function slider(){
+		echo '		
+	
+		';
+}
+
+
+function  footer(){
+	echo '
+
+	 <section class="contenido-centrado">
+    <article>
+    <article id="cont1">
+    <h1>ATENSA</h1>
+    <ul>
+    <li><a href="index.php">Incio</a></li>
+    <li><a href="quienes.php">Nosotros</a></li>
+	<li><a href="proyectos.php">Proyectos</a></li>
+    <li><a href="contacto.php">Contacto</a></li>
+	
+    </ul>
+    
+    
+    <h1>Facebook</h1>
+    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FATENSAsadecv%2F&tabs=timeline&width=200&height=230&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="200" height="230" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+     
+    </article>
+    
+
+
+    
+    <article id="cont1">
+    <h1>Proveedores</h1>
+    <ul>
+    <li><a href="productos-prov1.php" >Retex</a></li>
+    <li><a href="productos-prov4.php" >Lana mineral</a></li>
+    <li><a href="productos-prov2.php" >fibra de vidrio owens corning</a></li>			
+    <li><a href="productos-prov3.php" >Fibra cerámica</a></li>
+    <li><a href="productos-prov5.php" >Vidrio celular</a></li>
+    </ul>
+    </article>
+   
+    
+    <article id="cont1"  style="margin-left:15%"> 
+    
+    <h1>Datos de contacto:</h1>
+     <h3>Torreón</h3>
+    
+     <p><span>Telefono:<br> </span>
+	 871 7 32 85 09<br>
+	 871 7 32 23 95 <br>
+	 871 7 32 24 11  <br>
+	 </span>
+	 </p>
+     <p><span>Email:</span>
+     <br>
+     obras@atensa.com.mx<br>
+	 ventas@atensa.com.mx<br>
+	 atensa@hotmail.com<br>
+     fdovillagrana@atensa.com.mx<br>
+
+     </p>
+     
+     <h3>Durango </h3>
+     Ma. Bayona #601 Col. Santa fe 
+     Durango, Dgo.</br>
+     <p><span>Telefono: </span>
+     618 - 810-25-14</br>
+     
+    
+    </article>
+     
+	
+	<article id="cont1" > 
+     
+	<br><br><br><br>
+	<h3>Zona Bajio</h3>
+    
+     <p><span>Telefono: </span>462 107 40 33
+    
+    
+     </p>
+     <p><span>Email:</span>
+     <br>
+     e.raya@yahoo.com.mx<br>
+     
+    
+     </p>
+    
+	
+	</article>
+	
+    </article>
+    <article>
+    
+    <article style="width:50%; margin-top:1em;">
+    
+    <p>Desarrollado por <a href="http://www.awsoftware.mx/"><span>A W Software</span> </a></p>
+    
+    </article>
+
+    </article>
+    
+   
+    
+    </section>
+	';
+}
+?>
+
+ 
---
+++ fijosEng.php
@@ -0,0 +1,158 @@
+<?php
+
+//english page fijos
+function menures(){
+	echo '
+
+                  
+                  
+
+	';
+}
+
+
+function menu(){
+	echo '
+<section class="contenido-centrado">
+    <div id="logo">
+        <img src="img/log2.png"  alt=""/>
+    </div>
+    
+	<div id="cssmenu">
+	
+	          <ul >
+                <li><a href="index-eng.php">Home</a></li>
+				<li><a href="About.php">About us</a></li>
+                <li class="active has-sub"><a href="#" >Products</a>
+                <ul>
+                <li><a href="products-prov1-ENG.php" >Retex</a></li>
+                <li><a href="products-prov4-ENG.php" >Mineral Wool</a></li>
+                <li><a href="products-prov2-ENG.php" >Mineral Fiber </a></li>
+				
+                <li><a href="products-prov3-ENG.php" >Ceramic Fiber</a></li>
+                <li><a href="products-prov5-ENG.php" >Cellular Glass</a></li>
+                
+                </ul>
+                </li>
+				 <li><a href="projects.php" >Projects</a>
+                
+                <li><a href="contactus.php" >Contact us</a></li>
+                <li><a href="faqs-eng.php"> FAQs </a></li> 
+                <li><a href=index.php><img src="img/bmexico.png" width="30px" height="30px" ></a></li>
+                 <li><a href=index-eng.php><img src="img/busa.png" width="30px" height="30px" ></a></li>
+
+                
+                
+                </ul>
+	
+
+</div>
+	
+
+    </section>
+	';
+}
+
+function slider(){
+		echo '		
+	
+		';
+}
+
+
+function  footer(){
+	echo '
+
+	 <section class="contenido-centrado">
+    <article>
+    <article id="cont1">
+    <h1>ATENSA</h1>
+    <ul>
+    <li><a href="index-eng.php">Home</a></li>
+    <li><a href="About.php">About us</a></li>
+	<li><a href="projects.php">Projects</a></li>
+    <li><a href="contactus.php">Contact us</a></li>
+	
+    </ul>
+    
+     
+    <h1>Facebook</h1>
+    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FATENSAsadecv%2F&tabs=timeline&width=200&height=230&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="200" height="230" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+    </article>
+    
+    <article id="cont1">
+    <h1>Suppliers</h1>
+    <ul>
+    <li><a href="products-prov1-ENG.php" >Retex</a></li>
+    <li><a href="products-prov4-ENG.php" >Mineral Wool</a></li>
+    <li><a href="products-prov2-ENG.php" >Glass Fiber</a></li>			
+    <li><a href="products-prov3-ENG.php" >Ceramic Fiber</a></li>
+    <li><a href="products-prov5-ENG.php" >Cellular Glass</a></li>
+    </ul>
+    </article>
+   
+    
+    <article id="cont1"  style="margin-left:15%"> 
+    
+    <h1>Contact Information:</h1>
+     <h3>Torreón</h3>
+    
+     <p><span>Telephone:<br> </span>
+	 +52 871 7 32 85 09<br>
+	 +52 871 7 32 23 95 <br>
+	 +52 871 7 32 24 11  <br>
+	 </span>
+	 </p>
+     <p><span>Email:</span>
+     <br>
+     obras@atensa.com.mx<br>
+	 ventas@atensa.com.mx<br>
+	 atensa@hotmail.com<br>
+     fdovillagrana@atensa.com.mx<br>
+
+     </p>
+     
+     <h3>Durango </h3>
+     Ma. Bayona #601 Col. Santa fe 
+     Durango, Dgo.</br>
+     <p><span>Telephone: </span>
+     +52 618 - 810-25-14</br>
+     
+   
+    </article>
+	
+	<article id="cont1"  > 
+	<br><br><br><br>
+	<h3>Bajio Zone</h3>
+    
+     <p><span>Telephone: </span>+52 462 107 40 33
+    
+    
+     </p>
+     <p><span>Email:</span>
+     <br>
+     e.raya@yahoo.com.mx<br>
+    
+     </p>
+    
+	</article>
+	
+    </article>
+    <article>
+    
+    <article style="width:50%; margin-top:1em;">
+    
+    <p>Developed by <a href="http://www.awsoftware.mx/"><span>A W Software</span> </a></p>
+    
+    </article>
+
+    </article>
+    
+   
+    
+    </section>
+	';
+}
+?>
+
+ 
