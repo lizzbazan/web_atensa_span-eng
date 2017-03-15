b5dab1d6d44dcff97e71e97249f37d6af16ac8d9
Lizzeth Bazan <lizzbaz_1194@hotmail.com>
2017-03-15 17:29:51 -0600

    index 2 languages
    español, english

---
+++ index-eng.php
@@ -0,0 +1,122 @@
+<?php
+	include ("php/fijosEng.php");
+?>
+
+<!doctype html>
+
+<head>
+<meta charset="utf-8">
+
+<title>Atensa</title>
+<meta name="viewport" content="width=device-width initial-scale=1.0 maximum-scale=1.0 user-scalable=yes" />
+<meta name="description" content="En ATENSA nos especializamos en la comercialización de aislamientos térmicos para el hogar y la industria.">
+<meta name="keywords" content="ATENSA, Aislantes Termoacústicos del Norte, Aislamiento termico, Proveedor de aislamiento termico, RETEX, Impermeabilizantes Ecologicos, Morteros, Grouts, selladores, Waterstops, Aditivos, Productos para concreto, Recubrimientos decorativos impermeables, Aislamiento termico en tanques de proceso, Aislamiento termico en tuberias de vapor, Aislamiento termico en turbinas de vapor, Aislamiento termico reusable en turbina de gas, Aislamiento acustico en auditorios, Casetas acusticas, Instalacion de colcha en techo de nave MBI, Aislamiento termico en techo de casa habitacion hipoteca verde, Ownes Corning, El Aislacustic, Aislamiento termiacústico fabricado con fibra, Batts in bags, Aislamiento termoacústico fabricado con fibra, Ductboard, El fácil flex, Medias cañas, Flexi aerocel, Foamular, Metal building insultation, Propink, Quietr, Quietro Acoustic duct liner, Quietro duct board, Quietzone spiral duct liner, Torreón, Coahuila">
+<link href="img/favicon.png" rel="icon" />
+
+<link href="css/css.css" rel="stylesheet" type="text/css">
+<link href="css/cssmenu.css" rel="stylesheet" type="text/css">
+
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="js/script1.js"></script>
+
+
+
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<script type="text/javascript" src="js/jquery.js"></script>
+
+</head>
+
+<body>
+<section class="pagina">
+	<header>
+    <?php 
+	menu();
+	?>
+    </header>
+    
+    <section class="cabecera">
+    <div id="wowslider-container1">
+	<div class="ws_images">
+    <ul>
+    <li><img src="img/cabe1.jpg" alt="cabe1" title="<div>Reliable supplier in thermal and acoustic insulation</div>" id="wows1_0"/></li>
+    <li><img src="img/cabe2.jpg" alt="cabe2" title="<div>Reliable supplier in thermal and acoustic insulation</div>" id="wows1_1"/></li>
+    <li><img src="img/cabe3.jpg" alt="cabe3" title="<div>Reliable supplier in thermal and acoustic insulation</div>" id="wows1_2"/></li>
+    <li><img src="img/cabe4.jpg" alt="cabe4" title="<div>Reliable supplier in thermal and acoustic insulation</div>" id="wows1_3"/></li>
+    </ul>
+    </div>
+    <div class="ws_bullets"><div>
+    <a href="#" title="cabe1"><img src="data1/tooltips/cabe1.jpg" alt="cabe1"/>1</a>
+    <a href="#" title="cabe2"><img src="data1/tooltips/cabe2.jpg" alt="cabe2"/>2</a>
+    <a href="#" title="cabe3"><img src="data1/tooltips/cabe3.jpg" alt="cabe3"/>3</a>
+    <a href="#" title="cabe4"><img src="data1/tooltips/cabe4.jpg" alt="cabe4"/>4</a>
+    </div></div>
+    <span class="wsl"><a href="http://wowslider.net">slider html</a> by WOWSlider.com v5.5</span>
+	<div class="ws_shadow"></div>
+	</div>
+    
+    </section>
+    
+    <!-------------------------------contenido----------->
+    <section class="contenido" id="indexcont1">
+    <section class="contenido-centrado">
+    <article>
+    
+   <h1>Aislantes <br>Termoacústicos del Norte, S.A. de C.V.</h1>
+    
+    <p><?=_("Since 2001, ATENSA has been reliable supplier in quality, energy saving services, confort and industrial security in processing and construction industry.")?>:</p>
+    </article>
+    
+    <article>
+    <br><br>
+    <img src="img/img1.png" width="100%"  alt=""/> 
+    </article>
+
+    </section>
+    </section>
+    
+    <section class="contenido" id="indexcont2"> 
+    <section class="contenido-centrado">
+    
+    <article>
+   <h1><?=_("Our Company")?>:</h1>
+
+    <p><?=_("We are specialist in merchandising of termal insulation for industrial use.")?>:</p>
+    <p><?=_(
+    "We offer solutions that meet the expectations of our customers, always taking care of important details like quality, service, delivery time and price.
+    Our team is always available to provide the best service and always improve the quality of our work")?>:
+    </p>
+    
+    </article>
+    
+    <!--FORMATO PARA VIDEO RESPONSIVO-->
+	<div id="contenedor">
+    <iframe
+	width="200px"
+	height="200px"
+	src="https://www.youtube.com/embed/JY1-fdQsVAc"
+	frameborder="0"
+	allowfullscreen>
+	</iframe>
+	</div>
+	<!------------------------------------->
+    
+    </section>
+    </section>
+    
+    <!-------------------------------/contenido----------->
+    
+    <footer>
+    
+    <?php
+	footer();
+	?>
+    
+    </footer>
+    
+    
+</section>
+    
+  <script type="text/javascript" src="js/wowslider.js"></script>
+	<script type="text/javascript" src="js/script2.js"></script>  
+</body>
+</html>
